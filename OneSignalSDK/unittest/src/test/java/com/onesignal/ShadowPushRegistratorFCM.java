ed1d71a6ef6519424d7b9f1dfdce8d4bd4a917c3
mikechoch <mdicioccio25@gmail.com>
2020-04-15 21:22:42 -0700

    Android X migration for unit tests * Updated all android support to Android X libs * Updated all GCM to FCM across unit tests * Updated min sdk to 16 * Updated dependencies to min Android X   * com.google.android.gms:play-services-location:17.0.0   * com.google.firebase:firebase-core:17.2.2

--- OneSignalSDK/unittest/src/test/java/com/onesignal/ShadowPushRegistratorFCM.java
+++ OneSignalSDK/unittest/src/test/java/com/onesignal/ShadowPushRegistratorFCM.java
@@ -36,12 +36,11 @@ import org.robolectric.annotation.RealObject;
 import static org.robolectric.shadow.api.Shadow.directlyOn;
 
 @Implements(PushRegistratorAbstractGoogle.class)
-public class ShadowPushRegistratorGCM {
-
-    @RealObject
-    private PushRegistratorAbstractGoogle realInstance;
-
-    public static final String regId = "aspdfoh0fhj02hr-2h";
+public class ShadowPushRegistratorFCM {
+   
+   @RealObject private PushRegistratorAbstractGoogle realInstance;
+   
+   public static final String regId = "aspdfoh0fhj02hr-2h";
 
     public static boolean fail;
     public static boolean skipComplete;
