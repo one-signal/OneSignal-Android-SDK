1eeaf9bfb3a23ff5efefaf4a14c4d3268ab452f1
Josh Kasten <josh@onesignal.com>
2021-05-20 21:57:47 -0700

    Migrated off of ShadowApplication.getInstance()
    * This API was dropped in Roboelectric 4.4 so migrated to shadowOf()

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/TestHelpers.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/TestHelpers.java
@@ -61,6 +61,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.shadows.ShadowAlarmManager;
 import org.robolectric.shadows.ShadowApplication;
@@ -207,7 +208,7 @@ public class TestHelpers {
 
    // Run any OneSignal background threads including any pending runnables
    public static void threadAndTaskWait() throws Exception {
-      ShadowApplication.getInstance().getForegroundThreadScheduler().runOneTask();
+      shadowOf(RuntimeEnvironment.application).getForegroundThreadScheduler().runOneTask();
       // Runs Runnables posted by calling View.post() which are run on the main thread.
       Robolectric.getForegroundThreadScheduler().runOneTask();
 
