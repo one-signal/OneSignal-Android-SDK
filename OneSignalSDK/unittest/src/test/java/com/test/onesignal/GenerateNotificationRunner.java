0a45fde8f8489766a7e07ccc8400505a1be5e8e8
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-07-01 18:35:45 -0300

    Cancel Worker thread if notification nor processed
    * Notification might not be processed if it is a restored one or an IAM

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/GenerateNotificationRunner.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/GenerateNotificationRunner.java
@@ -1252,7 +1252,6 @@ public class GenerateNotificationRunner {
    @Test
    @Config(shadows = { ShadowGenerateNotification.class })
    public void shouldSendReceivedReceiptWhenEnabled() throws Exception {
-
       ShadowOneSignalRestClient.setRemoteParamsReceiveReceiptsEnable(true);
 
       String appId = "b2f7f966-d8cc-11e4-bed1-df8f05be55ba";
